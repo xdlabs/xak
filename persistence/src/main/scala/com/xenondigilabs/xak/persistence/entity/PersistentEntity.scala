e844f3833a2e5ecc44dd81c66c3dda318444d64b
Abhay Arora <belieflimitless@gmail.com>
2015-12-19 22:05:40 +0530

    Changed Persistence.

--- persistence/src/main/scala/com/xenondigilabs/xak/persistence/entity/PersistentEntity.scala
+++
@@ -1,9 +0,0 @@
-package com.xenondigilabs.xak.persistence.entity
-
-trait PersistentEntity {
-
-  protected var objectId: PersistentEntityIdentifier
-
-  def getObjectId(): PersistentEntityIdentifier
-
-}
